d426fac5d13fc0cc3a2c542c8220c82d038c8fbb
laurab14 <laurabidois14@gmail.com>
2019-04-26 09:01:15 +0200

    fin laura

--- panier.js
+++ panier.js
@@ -1,39 +1,62 @@
-$('button').click(function() { // ajout article sur le AJOUTER PANIER
+$('.btnPay').click(function() { // ajout article sur le AJOUTER PANIER
   var $this = $(this);
   var $article = $(this).closest('article'),
     nom = $article.find('.h5').html(),
     description = $article.data('ref'),
-    price = $article.data('price');
-    console.log(price);
+    price = Number($article.data('price'));
+  console.log(price);
+  var sousTotal = price;
 
-  $(function() {
-        $('li>.plus').click(function() {
-          $('#ajout').html(function(i, val) {
-            return ajout.value = val * 1 + 1;
-          });
-        });
-      });
-          $(function() {
-        $('li>.minus').click(function() {
-          $('#ajout').html(function(i, val) {
-            if (ajout.value >1){
-            return ajout.value = val * 1 - 1;
-            }
-            if (ajout.value = 1){
-              return ajout.value = 1;
-            }
-          });
-        });
-      });
+
   console.log('nom, description, prix : ' + nom, description, price);
   var $li = $('<li data-price="' + price + '"></li>');
-  $li.html(nom + '  Réf : ' + description + '  Prix :  ' + price + ' €' +  '<i class="minus fas fa-minus"></i>' + '<input id="ajout" type="text" value="1" size="1">' + '<i class="plus fas fa-plus"></i>' + '  <i class="deleted fas fa-trash-alt"></i>' + 'Sous total : ').appendTo($('#cart>ul'));
+  $li.html(nom + '  Réf : ' + description + '<i class="deleted fas fa-trash-alt"></i>' + '<br>' + 'Quantité :   ' + '<i class="moins fas fa-minus"></i>' + '<span id="ajout" >1</span>' + '<i class="plus fas fa-plus"></i></i>' + '<br>' + ' Prix :  ' + price + ' €' + '<br>' + 'Sous total : <span class="sousTotal"></span>' + '<br>' + '<p></p>').appendTo($('#cart>ul'));
 
-computePrice();
+  // console.log(quantity);
 
-if (current = 1){
-    $(this).unbind("click");
-}
+$('li>.plus').click(function() {
+    $('li>.calc>#ajout').html(function(){
+      var ajout = $(this).val();
+      return ajout * 1 + 1
+
+    });
+});
+
+  // $(function() {
+  //   $(document).on('click', '#cart>ul>li>.plus', function() {
+  //     $('#ajout').html(function(i, val) {
+  //       return val * 1 + 1;
+  //     });
+  //   });
+  // });
+
+  // $(document).on('click','#cart>ul>li>.plus', function() {
+  //   var quantity = $('#cart>ul>li>#ajout').val();
+  //   var quantity = Number(quantity);
+  //   quantity ++;
+  //   // $('#ajout').text(quantity);
+  //   var price = Number($article.data('price'));
+  //   var sousTotal = price * quantity;
+  //   // $('.sousTotal').text(sousTotal);
+  //
+  //
+  // });
+
+  //   $(document).on('click','#cart>ul>li>.plus', function() {
+  //   var quantity = $('#ajout').val();
+  //   var quantity = Number(quantity);
+  //   $('#ajout').text(quantity--);
+  //   var price = Number($article.data('price'));
+  //   var sousTotal = price * quantity;
+  //   $('.sousTotal').text(sousTotal);
+  //
+  // });
+
+  computePrice();
+  //
+  // if (current = 1) {
+  //   $(this).unbind("click");
+  // }
 
   $('#cart>ul>li>.deleted').click(function() { // Fonction sur la liste des articles
     $(this).parent().remove();
